0b01261b972eea114eb6843c5d407ce1e583471f
Vellito34 <alexgalvanglz7878@outlook.com>
2026-07-03 11:29:42 -0600

    se agrego CuentaUsuario y PlataformaStreaming classes

---
+++ src/CuentaUsuario.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+
+public abstract class CuentaUsuario {
+    private String correo;
+    private int meseActivo;
+    private PlanSuscripcion tipoPlan;
+
+    public CuentaUsuario(String correo, int meseActivo, PlanSuscripcion tipoPlan) {
+        this.correo = correo;
+        this.meseActivo = meseActivo;
+        this.tipoPlan = tipoPlan;
+    }
+
+    public void TotalAPagar(){
+        System.out.println(tipoPlan);
+    }
+
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getMeseActivo() {
+        return meseActivo;
+    }
+
+    public void setMeseActivo(int meseActivo) {
+        this.meseActivo = meseActivo;
+    }
+
+    public PlanSuscripcion getTipoPlan() {
+        return tipoPlan;
+    }
+
+    public void setTipoPlan(PlanSuscripcion tipoPlan) {
+        this.tipoPlan = tipoPlan;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        CuentaUsuario that = (CuentaUsuario) o;
+        return meseActivo == that.meseActivo && Objects.equals(correo, that.correo) && Objects.equals(tipoPlan, that.tipoPlan);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(correo, meseActivo, tipoPlan);
+    }
+}
---
+++ src/PlataformaStreaming.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class PlataformaStreaming {
+    ArrayList<CuentaUsuario> Usuarios = new ArrayList<>();
+    public void AgregarUsuario(CuentaUsuario u){
+        Usuarios.add(u);
+    }
+
+
+}
