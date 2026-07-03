6296279ad40f44c196b8054360e95f1b8f1b081d
Anthony De Alba (DAATony) <tony9mundial@gmail.com>
2026-07-03 11:27:40 -0600

    se crearon los planes con metodos propios

---
+++ src/PlanEstandar.java
@@ -0,0 +1,7 @@
+public class PlanEstandar implements PlanSuscripcion{
+
+    @Override
+    public double calcularCosto(int meses) {
+        return meses * 9.00;
+    }
+}
---
+++ src/PlanPremium.java
@@ -0,0 +1,7 @@
+public class PlanPremium implements PlanSuscripcion{
+
+    @Override
+    public double calcularCosto(int meses) {
+        return ((meses * 14.00) + 3.00);
+    }
+}
